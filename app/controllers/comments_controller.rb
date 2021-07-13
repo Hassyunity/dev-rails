edf18c220f198befe0f1de788d1757c0be16fb76
Hasyunity <hassy.tsihoarana@gmail.com>
2021-07-13 16:44:59 +0300

    .

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,5 @@
 class CommentsController < ApplicationController
-    http_basic_authenticate_with name: "name", password: "123456", except: [:index, :show]
+
     def create
         @article = Article.find(params[:article_id])
         @comment = @article.comments.create(comment_params)
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,5 @@ class Comment < ApplicationRecord
   
  
   belongs_to :article
+  belongs_to :comment
 end
