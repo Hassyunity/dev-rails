abf805c1f6d5e6f4d6e708311ab6fd99102c7d16
Hasyunity <hassy.tsihoarana@gmail.com>
2021-07-13 18:36:28 +0300

    modification comment parameters

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,7 @@
 class CommentsController < ApplicationController
 
+    http_basic_authenticate_with name: "name", password: "1234", except: [:index, :show]
+    
     def create
         @article = Article.find(params[:article_id])
         @comment = @article.comments.create(comment_params)
