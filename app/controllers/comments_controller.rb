acc386e1ed22dfe3f12fc6ed2e70fa728ef9b0d6
Hasyunity <hassy.tsihoarana@gmail.com>
2021-07-14 12:55:30 +0300

    ajout de haml

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,7 @@
 class CommentsController < ApplicationController
-
-    http_basic_authenticate_with name: "name", password: "1234", except: [:index, :show]
+    http_basic_authenticate_with name: "name", password: "1234", except: [:index, :show
     
-    def create
+        def create
         @article = Article.find(params[:article_id])
         @comment = @article.comments.create(comment_params)
         redirect_to article_path(@article)
